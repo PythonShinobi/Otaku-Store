6e9d7a12ce7695f69af4c347ec3596a0a552a981
PythonShinobi <pythonshinobi@gmail.com>
2024-07-08 08:25:28 +0300

    Connect to the database.

---
+++ server/api/db.js
@@ -0,0 +1,23 @@
+// server/api/db.js
+import pg from "pg";
+import env from "dotenv";
+
+env.config();
+
+const db = new pg.Client({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DB_PORT
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Could not connect to the database', err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+export default db;
