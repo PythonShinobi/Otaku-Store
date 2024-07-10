6ba083506b9da34508008638e3e663f98e434f47
PythonShinobi <pythonshinobi@gmail.com>
2024-07-10 10:59:38 +0300

    Add a comment explaining how the db works.

--- server/api/db.js
+++ server/api/db.js
@@ -4,6 +4,12 @@ import env from "dotenv";
 
 env.config();
 
+/**
+ * This code initializes a new PostgreSQL client using the `pg` library in Node.js.
+ * The client is configured to connect to a PostgreSQL database with credentials 
+ * and connection details sourced from environment variables. This setup allows 
+ * for secure and flexible database connection management.
+ */
 const db = new pg.Client({
   user: process.env.USER,
   host: process.env.HOST,
